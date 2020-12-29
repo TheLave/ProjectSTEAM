d3eeadde839f23a0d0e7bb1a69a3f58e5a163609
hijeu <74555910+hijeu@users.noreply.github.com>
2020-12-29 21:14:14 +0100

    Update AI_Sprint_2.py

--- AI_Sprint_2.py
+++ AI_Sprint_2.py
@@ -1,13 +1,20 @@
 import json
 from tkinter import *
+
 namenlijst_lengte = 1000
 
 with open('steam.json', 'r') as json_file:
     data = json.load(json_file)
 
+
+def programma_sluiten(event):
+    root.destroy()
+
+
 def eersteNaam():
     naam_eerste_spel = data[0]["name"]
     eerstenaam_tonen.config(text=f'{naam_eerste_spel}')
+
 
 def gesorteerd(sort):
     spellen_lijst.delete(0, 'end')
@@ -18,77 +25,82 @@ def gesorteerd(sort):
         spellen_lijst.insert(spel_nummer + 1, gesorteerd[spel_nummer])
         spel_nummer += 1
 
-root = Tk()
-frame = Frame(root)
 
-w, h = root.winfo_screenwidth(), root.winfo_screenheight()
-root.geometry("%dx%d+0+0" % (w, h))
-root.configure(bg='darkblue')
+# GUI (Tkinter)
+root = Tk()
+
+root.bind('<Escape>', programma_sluiten)
+
+root.attributes("-fullscreen", True)
+
+root.configure(background="darkblue")
+
+frame = Frame(master=root)
 
 titel = Label(master=root,
-    bg="darkblue",
-    fg="white",
-    text="Steamspellen",
-    font=('helvetica', 50, 'bold'))
+              background="darkblue",
+              foreground="white",
+              text="Steamspellen",
+              font=("helvetica", 50, "bold"))
 
 eerstenaam_tonen = Label(master=root,
-    bg='darkblue',
-    fg='white',
-    font=('helvetica', 15, 'bold'))
+                         background="darkblue",
+                         foreground="white",
+                         font=("helvetica", 15, "bold"))
 
 knop_eerstenaam = Button(master=root,
-    bg='blue2',
-    fg='white',
-    text="Laat eerste spel zien",
-    font=('helvetica', 15, 'bold'),
-    command=eersteNaam)
+                         background="blue2",
+                         foreground="white",
+                         text="Laat eerste spel zien",
+                         font=("helvetica", 15, "bold"),
+                         command=eersteNaam)
 
 knop_gesorteerd_appid = Button(master=root,
-    bg='blue2',
-    fg='white',
-    text="Sorteer spellen op appid",
-    font=('helvetica', 15, 'bold'),
-    command=lambda: gesorteerd('appid'))
+                               background="blue2",
+                               foreground="white",
+                               text="Sorteer spellen op appid",
+                               font=("helvetica", 15, "bold"),
+                               command=lambda: gesorteerd("appid"))
 
 knop_gesorteerd_naam =  Button(master=root,
-    bg='blue2',
-    fg='white',
-    text="Sorteer spellen op naam",
-    font=('helvetica', 15, 'bold'),
-    command=lambda: gesorteerd('name'))
+                               background="blue2",
+                               foreground="white",
+                               text="Sorteer spellen op naam",
+                               font=("helvetica", 15, "bold"),
+                               command=lambda: gesorteerd("name"))
 
 knop_gesorteerd_prijs = Button(master=root,
-    bg='blue2',
-    fg='white',
-    text="Sorteer spellen op prijs",
-    font=('helvetica', 15, 'bold'),
-    command=lambda: gesorteerd('price'))
+                               background="blue2",
+                               foreground="white",
+                               text="Sorteer spellen op prijs",
+                               font=("helvetica", 15, "bold"),
+                               command=lambda: gesorteerd("price"))
 
 knop_gesorteerd_datum = Button(master=root,
-    bg='blue2',
-    fg='white',
-    text="Sorteer spellen op datum",
-    font=('helvetica', 15, 'bold'),
-    command=lambda: gesorteerd('release_date'))
+                               background="blue2",
+                               foreground="white",
+                               text="Sorteer spellen op datum",
+                               font=("helvetica", 15, "bold"),
+                               command=lambda: gesorteerd("release_date"))
 
 spellen_lijst = Listbox(master=frame,
-    bg='darkblue',
-    fg='white',
-    bd=20,
-    height= 15,
-    width= 100,
-    font=('helvetica', 15,))
+                        background="darkblue",
+                        foreground="white",
+                        border=20,
+                        height=15,
+                        width=100,
+                        font=("helvetica", 15,))
 
-scroll_bar = Scrollbar(frame)
-scroll_barx = Scrollbar(frame, orient=HORIZONTAL)
+scroll_bar_y = Scrollbar(frame)
+scroll_bar_x = Scrollbar(frame, orient=HORIZONTAL)
 
-spellen_lijst.config(yscrollcommand=scroll_bar.set)
-scroll_bar.config(command=spellen_lijst.yview)
-spellen_lijst.config(xscrollcommand=scroll_barx.set)
-scroll_barx.config(command=spellen_lijst.xview)
+spellen_lijst.config(yscrollcommand=scroll_bar_y.set)
+scroll_bar_y.config(command=spellen_lijst.yview)
+spellen_lijst.config(xscrollcommand=scroll_bar_x.set)
+scroll_bar_x.config(command=spellen_lijst.xview)
 
-scroll_bar.pack(side="right", fill='y')
-scroll_barx.pack(side="bottom", fill='x')
+scroll_bar_y.pack(side="right", fill="y")
+scroll_bar_x.pack(side="bottom", fill="x")
 titel.pack(pady=40)
 eerstenaam_tonen.pack(pady=10)
 knop_eerstenaam.pack(pady=10)
