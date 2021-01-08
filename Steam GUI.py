7657bff6f93f12abb3a74e6d2ecc9bfa7ea35e92
hijeu <74555910+hijeu@users.noreply.github.com>
2021-01-08 22:32:27 +0100

    Update Steam GUI.py

--- Steam GUI.py
+++ Steam GUI.py
@@ -2,6 +2,13 @@ from tkinter import *
 import json
 
 
+class FrameMenubalk:
+    def __init__(self, master):
+        self.frame = Frame(master=master,
+                           background="#1f2124")
+        self.frame.pack(fill=X)
+
+
 class KnopMenubalk1:
     def __init__(self, master, text, command):
         self.button = Button(master=master,
@@ -35,101 +42,97 @@ class KnopMenubalk2:
 
 class SteamGUI:
     def __init__(self, master):
-        def store_pagina_tonen():
+        def store_scherm_tonen():
             knop_store.button.configure(foreground="#FFFFFF")
-            knop_1.button.configure(foreground="#a1aab8")
-            knop_2.button.configure(foreground="#a1aab8")
-            knop_3.button.configure(foreground="#a1aab8")
+            knop1.button.configure(foreground="#a1aab8")
+            knop2.button.configure(foreground="#a1aab8")
+            knop3.button.configure(foreground="#a1aab8")
 
-            frame_knop_1.forget()
-            frame_knop_2.forget()
-            frame_knop_3.forget()
+            frame_knop1.forget()
+            frame_knop2.forget()
+            frame_knop3.forget()
             frame_store.pack(fill=BOTH,
                              expand=TRUE)
 
 
-        def knop_1_pagina_tonen():
-            knop_1.button.configure(foreground="#FFFFFF")
-            knop_2.button.configure(foreground="#a1aab8")
-            knop_3.button.configure(foreground="#a1aab8")
+        def knop_1_scherm_tonen():
+            knop1.button.configure(foreground="#FFFFFF")
+            knop2.button.configure(foreground="#a1aab8")
+            knop3.button.configure(foreground="#a1aab8")
             knop_store.button.configure(foreground="#a1aab8")
 
-            frame_knop_2.forget()
-            frame_knop_3.forget()
+            frame_knop2.forget()
+            frame_knop3.forget()
             frame_store.forget()
-            frame_knop_1.pack(fill=BOTH,
+            frame_knop1.pack(fill=BOTH,
                               expand=TRUE)
 
 
-        def knop_2_pagina_tonen():
-            knop_2.button.configure(foreground="#FFFFFF")
-            knop_1.button.configure(foreground="#a1aab8")
-            knop_3.button.configure(foreground="#a1aab8")
+        def knop_2_scherm_tonen():
+            knop2.button.configure(foreground="#FFFFFF")
+            knop1.button.configure(foreground="#a1aab8")
+            knop3.button.configure(foreground="#a1aab8")
             knop_store.button.configure(foreground="#a1aab8")
 
-            frame_knop_1.forget()
-            frame_knop_3.forget()
+            frame_knop1.forget()
+            frame_knop3.forget()
             frame_store.forget()
-            frame_knop_2.pack(fill=BOTH,
-                              expand=TRUE)
+            frame_knop2.pack(fill=BOTH,
+                             expand=TRUE)
 
 
-        def knop_3_pagina_tonen():
-            knop_3.button.configure(foreground="#FFFFFF")
-            knop_1.button.configure(foreground="#a1aab8")
-            knop_2.button.configure(foreground="#a1aab8")
+        def knop_3_scherm_tonen():
+            knop3.button.configure(foreground="#FFFFFF")
+            knop1.button.configure(foreground="#a1aab8")
+            knop2.button.configure(foreground="#a1aab8")
             knop_store.button.configure(foreground="#a1aab8")
 
-            frame_knop_1.forget()
-            frame_knop_2.forget()
+            frame_knop1.forget()
+            frame_knop2.forget()
             frame_store.forget()
-            frame_knop_3.pack(fill=BOTH,
-                              expand=TRUE)
+            frame_knop3.pack(fill=BOTH,
+                             expand=TRUE)
 
 
-    # Widgets
+# Widgets
         hoofdframe = Frame(master=master,
                            background="#194761")
         hoofdframe.pack(fill=BOTH,
                         expand=TRUE)
 
         # Menubalk1
-        frame_menubalk1 = Frame(master=hoofdframe,
-                                background="#1f2124")
-        frame_menubalk1.pack(fill=X)
-
-        knop_programma_sluiten = KnopMenubalk1(frame_menubalk1, "x", master.quit)
-        knop_programma_minimaliseren = KnopMenubalk1(frame_menubalk1, "-", master.iconify)
+        frame_menubalk1 = FrameMenubalk(hoofdframe)
+        knop_programma_sluiten = KnopMenubalk1(frame_menubalk1.frame, "x", master.quit)
+        knop_programma_minimaliseren = KnopMenubalk1(frame_menubalk1.frame, "-", master.iconify)
 
         # Menubalk2
-        frame_menubalk2 = Frame(master=hoofdframe,
-                                background="#1f2124")
-        frame_menubalk2.pack(fill=X)
+        frame_menubalk2 = FrameMenubalk(hoofdframe)
+        knop_store = KnopMenubalk2(frame_menubalk2.frame, "Store", store_scherm_tonen)
+        knop1 = KnopMenubalk2(frame_menubalk2.frame, "Knop1", knop_1_scherm_tonen)
+        knop2 = KnopMenubalk2(frame_menubalk2.frame, "Knop2", knop_2_scherm_tonen)
+        knop3 = KnopMenubalk2(frame_menubalk2.frame, "Knop3", knop_3_scherm_tonen)
 
-        knop_store = KnopMenubalk2(frame_menubalk2, "Store", store_pagina_tonen)
-        knop_1 = KnopMenubalk2(frame_menubalk2, "Knop1", knop_1_pagina_tonen)
-        knop_2 = KnopMenubalk2(frame_menubalk2, "Knop2", knop_2_pagina_tonen)
-        knop_3 = KnopMenubalk2(frame_menubalk2, "Knop3", knop_3_pagina_tonen)
-
-        # Store pagina
+    # Schermen
+        # Store scherm
         frame_store = Frame(master=hoofdframe,
                             background="red")
 
-        # Knop 1 pagina
-        frame_knop_1 = Frame(master=hoofdframe,
+        # Knop1 scherm
+        frame_knop1 = Frame(master=hoofdframe,
                             background="green")
 
-        # Knop 2 pagina
-        frame_knop_2 = Frame(master=hoofdframe,
+        # Knop2 scherm
+        frame_knop2 = Frame(master=hoofdframe,
                             background="blue")
 
-        # Knop 3 pagina
-        frame_knop_3 = Frame(master=hoofdframe,
+        # Knop3 scherm
+        frame_knop3 = Frame(master=hoofdframe,
                             background="yellow")
 
         #TEST AREA
         # menu = Menu(master=master)
         # menu2 = Menubutton(master=master)
+
 
 def json_bestand_inlezen():
     with open("steam.json", "r") as json_file:
