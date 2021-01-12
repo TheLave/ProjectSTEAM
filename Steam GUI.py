85e371f9bb66d320ffca4c64ac8e95d27f78cdd3
hijeu <74555910+hijeu@users.noreply.github.com>
2021-01-13 00:20:09 +0100

    Update Steam GUI.py

--- Steam GUI.py
+++ Steam GUI.py
@@ -173,16 +173,70 @@ class SteamGUI:
                                  pady=2)
 
         # Constructors nog niet af
-        class ScaleStore:
-            def __init__(self):
-                pass
+        class FrameFilter:
+            def __init__(self, master):
+                self.frame = Frame(master=master,
+                                   background="#1b2837",
+                                   relief=RIDGE,
+                                   borderwidth=1)
+                self.frame.pack(pady=(0, 20))
 
+        class LabelFilter:
+            def __init__(self, master, background, text, anchor, pady):
+                self.label = Label(master=master,
+                                   foreground="white",
+                                   background=background,
+                                   width=30,
+                                   text=text,
+                                   font=("helvetica", 10),
+                                   anchor=anchor)
+                self.label.pack(pady=pady)
+
+        class ScaleStore:
+            def __init__(self, master, to, command):
+                self.scale = Scale(master=master,
+                                   troughcolor="#4c8fb4",
+                                   from_=0,
+                                   to=to,
+                                   length=200,
+                                   width=10,
+                                   sliderlength=20,
+                                   borderwidth=0,
+                                   showvalue=0,
+                                   orient=HORIZONTAL,
+                                   command=command)
+                self.scale.set(to)
+                self.scale.pack(pady=(15, 0))
+
+# Nog af te maken
         class CheckKnopStoreOpties:
             def __init__(self):
                 pass
 
         def applicatie_afsluiten(event):
             master.destroy()
+
+# Nog af te maken
+        def producten_tonen_op_price(gefilterde_prijs):
+            if int(gefilterde_prijs) == maximum_price:
+                label_gefilterde_price.label.configure(text="Any Price")
+            elif int(gefilterde_prijs) == 0:
+                label_gefilterde_price.label.configure(text=f"Free")
+            else:
+                label_gefilterde_price.label.configure(text=f"€{gefilterde_prijs},- & under")
+
+            getoonde_producten_sorteren(sort_by_optionmenu_waarde)
+
+# Nog af te maken
+        def producten_tonen_op_age(gefilterde_leeftijd):
+            if int(gefilterde_leeftijd) == maximum_age:
+                label_gefilterde_age.label.configure(text="Any Age")
+            elif int(gefilterde_leeftijd) == 0:
+                label_gefilterde_age.label.configure(text="0")
+            else:
+                label_gefilterde_age.label.configure(text=f"Age {gefilterde_leeftijd} & under")
+
+            getoonde_producten_sorteren(sort_by_optionmenu_waarde)
 
         def inhoud_listbox_aanpassen(lijst):
             """
@@ -202,14 +256,14 @@ class SteamGUI:
 
         lijst_temp = []
 
-        def getoonde_producten_sorteren(opties, waarde):
-            lijst = Sorteren.lijst_sorteren_op_optie(lijst_temp, opties, waarde)
+        def getoonde_producten_sorteren(waarde):
+            lijst = Sorteren.lijst_sorteren_op_optie(lijst_temp, sort_by_opties, waarde)
             inhoud_listbox_aanpassen(lijst)
 
-        def producten_tonen(opties, waarde):
+        def producten_tonen(waarde):
             zoekterm = entry_zoekbalk_producten.get()
             lijst = Sorteren.producten_zoeken_op_naam(data, lijst_temp, zoekterm)
-            lijst = Sorteren.lijst_sorteren_op_optie(lijst, opties, waarde)
+            lijst = Sorteren.lijst_sorteren_op_optie(lijst, sort_by_opties, waarde)
             inhoud_listbox_aanpassen(lijst)
 
         def geklikte_knop_menubalk2_highlighten(geklikte_knop, knoppen):
@@ -225,32 +279,36 @@ class SteamGUI:
             gekozen_pagina.pack(fill=BOTH,
                                 expand=TRUE)
 
-        def store_scherm_tonen(geklikte_knop, knoppen, gekozen_pagina, paginas):
-            geklikte_knop_menubalk2_highlighten(geklikte_knop, knoppen)
-            applicatie_pagina_tonen(gekozen_pagina, paginas)
+        def store_scherm_tonen(geklikte_knop, gekozen_pagina):
+            geklikte_knop_menubalk2_highlighten(geklikte_knop, knoppen_menubalk2)
+            applicatie_pagina_tonen(gekozen_pagina, applicatie_paginas)
+
+            sort_by_optionmenu_waarde.set(sort_by_opties[0])
 
             entry_zoekbalk_producten.delete(0, "end")
+            scale_filter_price.scale.set(maximum_price)
+            scale_filter_age.scale.set(maximum_age)
 
-            producten_tonen(sort_by_opties, sort_by_optionmenu_waarde)
+            producten_tonen(sort_by_optionmenu_waarde)
 
-        def knop_1_scherm_tonen(geklikte_knop, knoppen, gekozen_pagina, paginas):
-            geklikte_knop_menubalk2_highlighten(geklikte_knop, knoppen)
-            applicatie_pagina_tonen(gekozen_pagina, paginas)
+        def knop_1_scherm_tonen(geklikte_knop, gekozen_pagina):
+            geklikte_knop_menubalk2_highlighten(geklikte_knop, knoppen_menubalk2)
+            applicatie_pagina_tonen(gekozen_pagina, applicatie_paginas)
 
-        def knop_2_scherm_tonen(geklikte_knop, knoppen, gekozen_pagina, paginas):
-            geklikte_knop_menubalk2_highlighten(geklikte_knop, knoppen)
-            applicatie_pagina_tonen(gekozen_pagina, paginas)
+        def knop_2_scherm_tonen(geklikte_knop, gekozen_pagina):
+            geklikte_knop_menubalk2_highlighten(geklikte_knop, knoppen_menubalk2)
+            applicatie_pagina_tonen(gekozen_pagina, applicatie_paginas)
 
-        def knop_3_scherm_tonen(geklikte_knop, knoppen, gekozen_pagina, paginas):
-            geklikte_knop_menubalk2_highlighten(geklikte_knop, knoppen)
-            applicatie_pagina_tonen(gekozen_pagina, paginas)
+        def knop_3_scherm_tonen(geklikte_knop, gekozen_pagina):
+            geklikte_knop_menubalk2_highlighten(geklikte_knop, knoppen_menubalk2)
+            applicatie_pagina_tonen(gekozen_pagina, applicatie_paginas)
 
-        # Configs
+# Configs
         master.title("Steam")
         master.bind("<Escape>", applicatie_afsluiten)
         master.attributes("-fullscreen", True)
 
-        # Widgets
+# Widgets
         hoofdframe = Frame(master=master,
                            background="#1b2837")
         hoofdframe.pack(fill=BOTH,
@@ -263,10 +321,10 @@ class SteamGUI:
 
         # Menubalk2
         frame_menubalk2 = FrameMenubalk(hoofdframe)
-        knop_store = KnopMenubalk2(frame_menubalk2.frame, "STORE", lambda: store_scherm_tonen(knop_store, knoppen_menubalk2, frame_store, applicatie_paginas))
-        knop1 = KnopMenubalk2(frame_menubalk2.frame, "KNOP1", lambda: knop_1_scherm_tonen(knop1, knoppen_menubalk2, frame_knop1, applicatie_paginas))
-        knop2 = KnopMenubalk2(frame_menubalk2.frame, "KNOP2", lambda: knop_2_scherm_tonen(knop2, knoppen_menubalk2, frame_knop2, applicatie_paginas))
-        knop3 = KnopMenubalk2(frame_menubalk2.frame, "KNOP3", lambda: knop_3_scherm_tonen(knop3, knoppen_menubalk2, frame_knop3, applicatie_paginas))
+        knop_store = KnopMenubalk2(frame_menubalk2.frame, "STORE", lambda: store_scherm_tonen(knop_store, frame_store))
+        knop1 = KnopMenubalk2(frame_menubalk2.frame, "KNOP1", lambda: knop_1_scherm_tonen(knop1, frame_knop1))
+        knop2 = KnopMenubalk2(frame_menubalk2.frame, "KNOP2", lambda: knop_2_scherm_tonen(knop2, frame_knop2))
+        knop3 = KnopMenubalk2(frame_menubalk2.frame, "KNOP3", lambda: knop_3_scherm_tonen(knop3, frame_knop3))
 
         knoppen_menubalk2 = []
         knoppen_menubalk2.extend([knop_store,
@@ -274,7 +332,7 @@ class SteamGUI:
                                   knop2,
                                   knop3])
 
-        # Schermen
+    # Schermen
         # Store scherm
         frame_store = Frame(master=hoofdframe,
                             background="#1b2837")
@@ -283,7 +341,7 @@ class SteamGUI:
                                       background="#1b2837")
         frame_store_producten.pack(side=LEFT,
                                    padx=(240, 10))
-
+            # Zoekbalk
         frame_zoekbalk_producten = Frame(master=frame_store_producten,
                                          background="#101822")
         frame_zoekbalk_producten.pack(padx=10,
@@ -306,9 +364,10 @@ class SteamGUI:
                                          width=8,
                                          text="Search",
                                          font=("helvetica", 10, "bold"),
-                                         command=lambda: producten_tonen(sort_by_opties, sort_by_optionmenu_waarde))
+                                         command=lambda: producten_tonen(sort_by_optionmenu_waarde))
         knop_zoekbalk_producten.pack(side=LEFT)
 
+            # Sort by
         label_sort_by = Label(master=frame_zoekbalk_producten,
                               foreground="#2f475e",
                               background="#101822",
@@ -342,8 +401,9 @@ class SteamGUI:
         optionmenu_sort_by.pack(side=LEFT,
                                 padx=10)
 
-        sort_by_optionmenu_waarde.trace("w", lambda i, j, k: getoonde_producten_sorteren(sort_by_opties, sort_by_optionmenu_waarde))
+        sort_by_optionmenu_waarde.trace("w", lambda *args: getoonde_producten_sorteren(sort_by_optionmenu_waarde))
 
+            # Listbox
         frame_listbox_producten = Frame(master=frame_store_producten,
                                         background="#1b2837")
         frame_listbox_producten.pack()
@@ -364,9 +424,29 @@ class SteamGUI:
 
         listbox_producten.configure(yscrollcommand=scroll_bar_y_listbox_producten.set)
 
-        frame_store_opties = Frame(master=frame_store,
-                                   background="#1b2837")
-        frame_store_opties.pack(side=RIGHT)
+        frame_store_filters = Frame(master=frame_store,
+                                    background="#1b2837")
+        frame_store_filters.pack(side=LEFT)
+
+            # Scales
+        maximum_price = 60
+        maximum_age = 18
+
+        frame_store_filters_price = FrameFilter(frame_store_filters)
+        label_filter_narrow_by_price = LabelFilter(frame_store_filters_price.frame, "#323e4b", "Narrow by Price", W, 0)
+        scale_filter_price = ScaleStore(frame_store_filters_price.frame, maximum_price, lambda gefilterde_prijs: producten_tonen_op_price(gefilterde_prijs))
+        label_gefilterde_price = LabelFilter(frame_store_filters_price.frame, "#1b2837", "Any Price", CENTER, 10)
+
+        frame_store_filters_age = FrameFilter(frame_store_filters)
+        label_filter_narrow_by_age = LabelFilter(frame_store_filters_age.frame, "#323e4b", "Narrow by Age", W, 0)
+        scale_filter_age = ScaleStore(frame_store_filters_age.frame, maximum_age, lambda gefilterde_leeftijd: producten_tonen_op_age(gefilterde_leeftijd))
+        label_gefilterde_age = LabelFilter(frame_store_filters_age.frame, "#1b2837", "Any Age", CENTER, 10)
+
+        frame_store_filters_tags = FrameFilter(frame_store_filters)
+        label_filter_tags = LabelFilter(frame_store_filters_tags.frame, "#323e4b", "Narrow by Tag", W, 0)
+
+            # Check boxes
+        # Hier komen de check boxes.
 
         # Knop1 scherm
         frame_knop1 = Frame(master=hoofdframe,
@@ -387,8 +467,8 @@ class SteamGUI:
                                    frame_knop2,
                                    frame_knop3])
 
-        # Store tonen bij het opstarten van de applicatie.
-        store_scherm_tonen(knop_store, knoppen_menubalk2, frame_store, applicatie_paginas)
+    # Store tonen bij het opstarten van de applicatie.
+        store_scherm_tonen(knop_store, frame_store)
 
 
 def main():
@@ -404,11 +484,10 @@ if __name__ == "__main__":
     main()
 
 # To do:
-        # Zorgen dat listbox bij default alle producten toont gesorteerd op rating                                                                   DONE
+        # Zorgen dat listbox bij default alle producten toont gesorteerd op rating                                                                       DONE
         # - Dropdown menu for Sort by                                                                                                                    DONE
-        # - Scale for maximum price
-        # - Scale for minimum price
-        # - Scale for required_age
+        # - Scale for price                                                                                                                              ALMOST DONE 
+        # - Scale for required_age                                                                                                                       ALMOST DONE
         # - Check buttons for platforms
         # - Check buttons for steamspy_tags
         # - Check buttons for genres
